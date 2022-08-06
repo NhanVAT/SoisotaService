c92d308150c114162de6db962efb422150995e25
nhanqbu <nhanqbu@gmail.com>
2022-08-06 20:05:57 +0700

    Initial commit

---
+++ src/main/java/cfm/SoisotaService/SoisotaServiceApplication.java
@@ -0,0 +1,13 @@
+package cfm.SoisotaService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoisotaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoisotaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cfm/SoisotaService/SoisotaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cfm.SoisotaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoisotaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
